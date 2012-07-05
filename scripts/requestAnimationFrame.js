da4aed8afca9c252057407898d7e66d9e39b5e64
sergio <sergio@sergiotorre>
2012-07-05 19:47:31 +0200

    Presentación y formato de requestAnimationFrame.js y licencia.

--- scripts/requestAnimationFrame.js
+++ scripts/requestAnimationFrame.js
@@ -1,23 +1,67 @@
+/*
+                                ESPAÑOL
+
+  Este archivo es parte de 'Gravedad-mini-ejemplo'.
+
+  Copyright 2012 Sergio Lindo - <sergiolindo.empresa@gmail.com>
+
+  'Gravedad-mini-ejemplo' es software libre: usted puede redistribuirlo y/o
+  modificarlo bajo los términos de la Licencia Pública General GNU publicada
+  por la Fundación para el Software Libre, ya sea la versión 3 de la Licencia,
+  o (a su elección) cualquier versión posterior.
+
+  'Gravedad-mini-ejemplo' se distribuye con la esperanza de que sea útil, pero
+  SIN GARANTÍA ALGUNA; ni siquiera la garantía implícita MERCANTIL o de
+  APTITUD PARA UN PROPÓSITO DETERMINADO. Consulte los detalles de la Licencia
+  Pública General GNU para obtener una información más detallada.
+
+  Debería haber recibido una copia de la Licencia Pública General GNU junto a
+  'Gravedad-mini-ejemplo'. En caso contrario, consulte
+  <http://www.gnu.org/licenses/>.
+
+
+                                ENGLISH
+
+  This file is part of 'Gravedad-mini-ejemplo'.
+
+  Copyright 2012 Sergio Lindo - <sergiolindo.empresa@gmail.com>
+
+  'Gravedad-mini-ejemplo' is free software: you can redistribute it and/or
+  modify it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or (at your
+  option) any later version.
+
+  'Gravedad-mini-ejemplo' is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+  Public License for more details.
+
+  You should have received a copy of the GNU General Public License along with
+  'Gravedad-mini-ejemplo'. If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
 // Bucle optimizado para animaciones web.
 (function() {
-  var lastTime = 0;
-  var vendors = ['ms', 'moz', 'webkit', 'o'];
+	var lastTime = 0;
+	var vendors = ['ms', 'moz', 'webkit', 'o'];
 
-  for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
-   window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
-   window.cancelAnimationFrame = window[vendors[x]+'CancelAnimationFrame'] || window[vendors[x]+'CancelRequestAnimationFrame'];
-  }
+	for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+		window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
+		window.cancelAnimationFrame = window[vendors[x]+'CancelAnimationFrame'] ||
+									  window[vendors[x]+'CancelRequestAnimationFrame'];
+	}
 
-  if (!window.requestAnimationFrame)
-   window.requestAnimationFrame = function(callback, element) {
-    var currTime = new Date().getTime();
-    var timeToCall = Math.max(0, 16 - (currTime - lastTime));
-    var id = window.setTimeout(function() { callback(currTime + timeToCall); }, timeToCall);
-    lastTime = currTime + timeToCall;
-    return id;
-   };
+	if (!window.requestAnimationFrame)
+		window.requestAnimationFrame = function(callback, element) {
+			var currTime = new Date().getTime();
+			var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+			var id = window.setTimeout(function() { callback(currTime + timeToCall); }, timeToCall);
+			lastTime = currTime + timeToCall;
+			return id;
+	};
 
-  if (!window.cancelAnimationFrame)
-   window.cancelAnimationFrame = function(id) { clearTimeout(id); };
+	if (!window.cancelAnimationFrame)
+		window.cancelAnimationFrame = function(id) { clearTimeout(id); };
 }());
 
